1317e2cac782c0f7c5bb6d4c7348f8361200c598
Thiha Htet Zaw <thihahtetzaw7991@gmail.com>
2024-11-01 20:57:56 +0630

    add crud

--- src/main.js
+++ src/main.js
@@ -3,10 +3,13 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import 'primeicons/primeicons.css'
+import toast from 'vue-toastification'
+import 'vue-toastification/dist/index.css'
 import router from './router'
 
 const app = createApp(App)
 
-app.use(router)
+app.use(router);
+app.use(toast);
 
 app.mount('#app')
